773eb19f6c09164e47f3e04d784c6ab8873b02ef
Zongwei Zhou <zzhou82@jh.edu>
2022-08-05 01:52:41 -0400

    Update files

--- .ipynb_checkpoints/utils-checkpoint.py
+++ .ipynb_checkpoints/utils-checkpoint.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from random import sample
 from tqdm import tqdm
-from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix, precision_score, f1_score
+from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix, precision_score, f1_score, jaccard_score
 
 THRESHOLD = 0.5
 SAMPLE_TIME = 100
@@ -10,7 +10,7 @@ SAMPLE_RATIO = 0.5
 
 def sampling_dataset(gt, predict):
     metrics = {}
-    metrics['roc_auc_score'], metrics['precision_score'], metrics['f1_score'] = [], [], []
+    metrics['roc_auc_score'], metrics['precision_score'], metrics['f1_score'], metrics['jaccard_score'] = [], [], [], []
     metrics['sensitivity_score'], metrics['specificity_score'] = [], []
 
     for _ in tqdm(range(SAMPLE_TIME)):
@@ -18,6 +18,7 @@ def sampling_dataset(gt, predict):
         metrics['roc_auc_score'].append(roc_auc_score(gt[rand_index], predict[rand_index]>THRESHOLD))
         metrics['precision_score'].append(precision_score(gt[rand_index], predict[rand_index]>THRESHOLD))
         metrics['f1_score'].append(f1_score(gt[rand_index], predict[rand_index]>THRESHOLD))
+        metrics['jaccard_score'].append(jaccard_score(gt[rand_index], predict[rand_index]>THRESHOLD))
 
         tn, fp, fn, tp = confusion_matrix(gt[rand_index], predict[rand_index]>THRESHOLD).ravel()
         metrics['sensitivity_score'].append(tp/(tp+fn))
